e6709910e2686503d85f1ddda90ff1abd0f4fa14
JuanMon24 <juandamc2020@gmail.com>
2021-07-01 13:39:06 -0500

    adding form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('hola mundo con Parcel.js');
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector('form')
+  
+    form.addEventListener('submit', function(event) {
+      event.preventDefault()
+  
+      const inputs = document.querySelectorAll('input, textarea')
+  
+      const data = [...inputs].reduce((prev, cur) => {
+          prev[cur.id] =  cur.value
+          return prev
+      }, {})
+  
+      alert(JSON.stringify(data))
+    });
+  });
